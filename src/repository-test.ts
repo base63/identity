28a9a64a3e8c461c1d7f5bac7d21f44313a0412e
Horia Coman <horia141@gmail.com>
2017-11-12 09:50:37 +0200

    More setups for a better build

--- src/repository-test.ts
+++ src/repository-test.ts
@@ -2,6 +2,8 @@ import { expect } from 'chai'
 import * as knex from 'knex'
 import 'mocha'
 
+import { startupMigration } from '@base63/common-server-js'
+
 import * as config from './config'
 import { Repository } from './repository'
 
@@ -9,6 +11,10 @@ import { Repository } from './repository'
 describe('Repository', () => {
     let conn: knex|null;
     const rightNow: Date = new Date(Date.now());
+
+    before('setup database', () => {
+        startupMigration();
+    });
 
     before('create connection', () => {
         conn = knex({
@@ -26,6 +32,14 @@ describe('Repository', () => {
         (conn as knex).destroy();
     });
 
+    afterEach('clear out database', () => {
+        const theConn = conn as knex;
+        theConn('identity.user_events').delete();
+        theConn('identity.user').delete();
+        theConn('identity.session_events').delete();
+        theConn('identity.session').delete();
+    });
+
     it('can be created', () => {
         const repository = new Repository(conn as knex);
         expect(repository).is.not.null;
@@ -40,5 +54,19 @@ describe('Repository', () => {
             expect(sessionToken).is.not.null;
             expect(session).is.not.null;
         });
+
+        // it('should reuse an already existing token', async () => {
+        //     const repository = new Repository(conn as knex);
+        //     const [sessionToken, session, created] = await repository.getOrCreateSession(null, rightNow);
+        //     const [newSessionToken, newSession, newCreated] = await repository.getOrCreateSession(sessionToken, rightNow);
+
+        //     expect(created).to.be.true;
+        //     expect(sessionToken).is.not.null;
+        //     expect(session).is.not.null;
+        //     expect(newSessionToken).is.not.null;
+        //     expect(newSessionToken).to.eql(sessionToken);
+        //     expect(newSession).to.eql(session);
+        //     expect(newCreated).to.be.false;
+        // });
     });
 });
