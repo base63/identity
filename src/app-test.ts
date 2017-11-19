df8031626321ca0c51d7b71d8f3258c4e6d8e406
Horia Coman <horia141@gmail.com>
2017-11-19 16:37:09 +0200

    Tests for the session delete

--- src/app-test.ts
+++ src/app-test.ts
@@ -1,5 +1,6 @@
 import * as auth0 from 'auth0'
 import { expect } from 'chai'
+import * as express from 'express'
 import * as HttpStatus from 'http-status-codes'
 import 'mocha'
 import { MarshalFrom } from 'raynor'
@@ -8,13 +9,17 @@ import { agent, Test } from 'supertest'
 import * as uuid from 'uuid'
 
 import { Env } from '@base63/common-js'
-import { SESSION_TOKEN_COOKIE_NAME, SESSION_TOKEN_HEADER_NAME } from '@base63/identity-sdk-js/client'
+import {
+    SESSION_TOKEN_COOKIE_NAME,
+    SESSION_TOKEN_HEADER_NAME,
+    XSRF_TOKEN_HEADER_NAME
+} from '@base63/identity-sdk-js/client'
 import { SessionAndTokenResponse, SessionResponse } from '@base63/identity-sdk-js/dtos'
 import { Session, SessionState } from '@base63/identity-sdk-js/entities'
 import { SessionToken } from '@base63/identity-sdk-js/session-token'
 
 import { AppConfig, newApp } from './app'
-import { Repository, SessionNotFoundError } from './repository'
+import { Repository, SessionNotFoundError, XsrfTokenMismatchError } from './repository'
 
 
 describe('App', () => {
@@ -204,29 +209,91 @@ describe('App', () => {
         badSessionToken('/session', 'get');
         badRepository('/session', 'get', { getSession: (_t: SessionToken) => { } }, {
             'NOT_FOUND when the session is not present': [new SessionNotFoundError('Not found'), HttpStatus.NOT_FOUND],
-            'INTERNAL_SERVER_ERROR when the repository errors': [new Error('An error occured'), HttpStatus.INTERNAL_SERVER_ERROR]
+            'INTERNAL_SERVER_ERROR when the repository errors': [new Error('An error occurred'), HttpStatus.INTERNAL_SERVER_ERROR]
         });
     });
 
-    function badOrigins(uri: string, method: 'post' | 'get') {
+    describe('/session DELETE', () => {
+        it('should succeed when the session token is in the cookie', async () => {
+            const auth0Client = td.object({});
+            const repository = td.object({
+                removeSession: (_t: SessionToken, _d: Date, _x: string) => { }
+            });
+
+            const app = newApp(localAppConfig, auth0Client as auth0.AuthenticationClient, repository as Repository);
+            const appAgent = agent(app);
+
+            td.when(repository.removeSession(theSessionToken, td.matchers.isA(Date), theSession.xsrfToken)).thenReturn();
+
+            await appAgent
+                .delete('/session')
+                .set('Cookie', `${SESSION_TOKEN_COOKIE_NAME}=${JSON.stringify(sessionTokenMarshaller.pack(theSessionToken))}`)
+                .set(XSRF_TOKEN_HEADER_NAME, theSession.xsrfToken)
+                .set('Origin', 'core')
+                .expect('Content-Type', 'application/json; charset=utf-8')
+                .expect(HttpStatus.NO_CONTENT)
+                .then(response => {
+                    expect(response.text).to.have.length(0);
+                });
+        });
+
+        it('should succeed when the session token is in the header', async () => {
+            const auth0Client = td.object({});
+            const repository = td.object({
+                removeSession: (_t: SessionToken, _d: Date, _x: string) => { }
+            });
+
+            const app = newApp(localAppConfig, auth0Client as auth0.AuthenticationClient, repository as Repository);
+            const appAgent = agent(app);
+
+            td.when(repository.removeSession(theSessionToken, td.matchers.isA(Date), theSession.xsrfToken)).thenReturn();
+
+            await appAgent
+                .delete('/session')
+                .set(SESSION_TOKEN_HEADER_NAME, JSON.stringify(sessionTokenMarshaller.pack(theSessionToken)))
+                .set(XSRF_TOKEN_HEADER_NAME, theSession.xsrfToken)
+                .set('Origin', 'core')
+                .expect('Content-Type', 'application/json; charset=utf-8')
+                .expect(HttpStatus.NO_CONTENT)
+                .then(response => {
+                    expect(response.text).to.have.length(0);
+                });
+        });
+
+        badOrigins('/session', 'delete');
+        badSessionToken('/session', 'delete');
+        badXsrfToken('/session', 'delete');
+        badRepository('/session', 'delete', { removeSession: (_t: SessionToken, _d: Date, _x: string) => { } }, {
+            'NOT_FOUND when the session is not present': [new SessionNotFoundError('Not found'), HttpStatus.NOT_FOUND],
+            'BAD_REQUEST when the XSRF token is mismatched': [new XsrfTokenMismatchError('Invalid token'), HttpStatus.BAD_REQUEST],
+            'INTERNAL_SERVER_ERROR when the repository errors': [new Error('An error occurred'), HttpStatus.INTERNAL_SERVER_ERROR]
+        });
+    });
+
+    type Method = 'post' | 'get' | 'delete';
+
+    function newAgent(app: express.Express, uri: string, method: Method): Test {
+        const appAgent = agent(app);
+
+        switch (method) {
+            case 'post':
+                return appAgent.post(uri);
+            case 'get':
+                return appAgent.get(uri);
+            case 'delete':
+                return appAgent.delete(uri);
+        }
+    }
+
+    function badOrigins(uri: string, method: Method) {
         it('should return BAD_REQUEST when there is no origin', async () => {
             const auth0Client = td.object({});
             const repository = td.object({});
 
             const app = newApp(localAppConfig, auth0Client as auth0.AuthenticationClient, repository as Repository);
-            const appAgent = agent(app);
-            let restOfTest;
+            const restOfTest = newAgent(app, uri, method);
 
-            switch (method) {
-                case 'post':
-                    restOfTest = appAgent.post(uri);
-                    break;
-                case 'get':
-                    restOfTest = appAgent.get(uri);
-                    break;
-            }
-
-            await (restOfTest as Test)
+            await restOfTest
                 .expect(HttpStatus.BAD_REQUEST)
                 .then(response => {
                     expect(response.text).to.have.length(0);
@@ -238,19 +305,9 @@ describe('App', () => {
             const repository = td.object({});
 
             const app = newApp(localAppConfig, auth0Client as auth0.AuthenticationClient, repository as Repository);
-            const appAgent = agent(app);
-            let restOfTest;
+            const restOfTest = newAgent(app, uri, method);
 
-            switch (method) {
-                case 'post':
-                    restOfTest = appAgent.post(uri);
-                    break;
-                case 'get':
-                    restOfTest = appAgent.get(uri);
-                    break;
-            }
-
-            await (restOfTest as Test)
+            await restOfTest
                 .set('Origin', 'bad-origin')
                 .expect(HttpStatus.BAD_REQUEST)
                 .then(response => {
@@ -259,26 +316,15 @@ describe('App', () => {
         });
     }
 
-    function badSessionToken(uri: string, method: 'post' | 'get') {
+    function badSessionToken(uri: string, method: Method) {
         it('should return BAD_REQUEST when there is no session token', async () => {
             const auth0Client = td.object({});
             const repository = td.object({});
 
             const app = newApp(localAppConfig, auth0Client as auth0.AuthenticationClient, repository as Repository);
+            const restOfTest = newAgent(app, uri, method);
 
-            const appAgent = agent(app);
-            let restOfTest;
-
-            switch (method) {
-                case 'post':
-                    restOfTest = appAgent.post(uri);
-                    break;
-                case 'get':
-                    restOfTest = appAgent.get(uri);
-                    break;
-            }
-
-            await (restOfTest as Test)
+            await restOfTest
                 .set('Origin', 'core')
                 .expect(HttpStatus.BAD_REQUEST)
                 .then(response => {
@@ -291,19 +337,9 @@ describe('App', () => {
             const repository = td.object({});
 
             const app = newApp(localAppConfig, auth0Client as auth0.AuthenticationClient, repository as Repository);
-            const appAgent = agent(app);
-            let restOfTest;
+            const restOfTest = newAgent(app, uri, method);
 
-            switch (method) {
-                case 'post':
-                    restOfTest = appAgent.post(uri);
-                    break;
-                case 'get':
-                    restOfTest = appAgent.get(uri);
-                    break;
-            }
-
-            await (restOfTest as Test)
+            await restOfTest
                 .set('Cookie', `${SESSION_TOKEN_COOKIE_NAME}=badtoken`)
                 .set(SESSION_TOKEN_HEADER_NAME, JSON.stringify(sessionTokenMarshaller.pack(theSessionToken))) // Doesn't matter we have the good one here
                 .set('Origin', 'core')
@@ -318,19 +354,9 @@ describe('App', () => {
             const repository = td.object({});
 
             const app = newApp(localAppConfig, auth0Client as auth0.AuthenticationClient, repository as Repository);
-            const appAgent = agent(app);
-            let restOfTest;
+            const restOfTest = newAgent(app, uri, method);
 
-            switch (method) {
-                case 'post':
-                    restOfTest = appAgent.post(uri);
-                    break;
-                case 'get':
-                    restOfTest = appAgent.get(uri);
-                    break;
-            }
-
-            await (restOfTest as Test)
+            await restOfTest
                 .set(SESSION_TOKEN_HEADER_NAME, 'bad token')
                 .set('Origin', 'core')
                 .expect(HttpStatus.BAD_REQUEST)
@@ -340,7 +366,42 @@ describe('App', () => {
         });
     }
 
-    function badRepository(uri: string, method: 'post' | 'get', repositoryTemplate: object, cases: Map<string, [Error, number]>) {
+    function badXsrfToken(uri: string, method: Method) {
+        it('should return BAD_REQUEST when the xsrf token is missing', async () => {
+            const auth0Client = td.object({});
+            const repository = td.object({});
+
+            const app = newApp(localAppConfig, auth0Client as auth0.AuthenticationClient, repository as Repository);
+            const restOfTest = newAgent(app, uri, method);
+
+            await restOfTest
+                .set(SESSION_TOKEN_HEADER_NAME, JSON.stringify(sessionTokenMarshaller.pack(theSessionToken)))
+                .set('Origin', 'core')
+                .expect(HttpStatus.BAD_REQUEST)
+                .then(response => {
+                    expect(response.text).has.length(0);
+                });
+        });
+
+        it('should return BAD_REQUEST when the xsrf token is invalid', async () => {
+            const auth0Client = td.object({});
+            const repository = td.object({});
+
+            const app = newApp(localAppConfig, auth0Client as auth0.AuthenticationClient, repository as Repository);
+            const restOfTest = newAgent(app, uri, method);
+
+            await restOfTest
+                .set(SESSION_TOKEN_HEADER_NAME, JSON.stringify(sessionTokenMarshaller.pack(theSessionToken)))
+                .set(XSRF_TOKEN_HEADER_NAME, 'A BAD TOKEN')
+                .set('Origin', 'core')
+                .expect(HttpStatus.BAD_REQUEST)
+                .then(response => {
+                    expect(response.text).has.length(0);
+                });
+        })
+    }
+
+    function badRepository(uri: string, method: Method, repositoryTemplate: object, cases: Map<string, [Error, number]>) {
         for (let oneCase of Object.keys(cases)) {
             const methodName = Object.keys(repositoryTemplate)[0]
             const [error, statusCode] = (cases as any)[oneCase]; // sigh
@@ -349,22 +410,13 @@ describe('App', () => {
                 const repository = td.object(repositoryTemplate);
 
                 const app = newApp(localAppConfig, auth0Client as auth0.AuthenticationClient, repository as Repository);
-                const appAgent = agent(app);
-                let restOfTest;
-
-                switch (method) {
-                    case 'post':
-                        restOfTest = appAgent.post(uri);
-                        break;
-                    case 'get':
-                        restOfTest = appAgent.get(uri);
-                        break;
-                }
+                const restOfTest = newAgent(app, uri, method);
 
                 td.when((repository as any)[methodName](), { ignoreExtraArgs: true }).thenThrow(error);
 
                 await (restOfTest as Test)
                     .set(SESSION_TOKEN_HEADER_NAME, JSON.stringify(sessionTokenMarshaller.pack(theSessionToken)))
+                    .set(XSRF_TOKEN_HEADER_NAME, theSession.xsrfToken)
                     .set('Origin', 'core')
                     .expect(statusCode)
                     .then(response => {
